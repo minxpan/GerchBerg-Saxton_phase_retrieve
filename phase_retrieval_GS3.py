68a516ba6663bfd101e47b082fc078a48b0e4a5c
minxpan <61602133+minxpan@users.noreply.github.com>
2020-04-28 10:53:57 -0400

    Add files via upload

--- phase_retrieval_GS3.py
+++ phase_retrieval_GS3.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 
+from phase_generation import phase_geNumumerator
+
 
 def Ger_Sax_algo(imgNear, imgFar, max_iter):
     h, w = imgNear.shape
@@ -19,6 +21,7 @@ def Ger_Sax_algo(imgNear, imgFar, max_iter):
         signal_f = am_f * np.exp(pm_f * 1j)
         signal_s = np.fft.ifft2(signal_f)
         pm_s = np.angle(signal_s)
+        pm_s = phase_geNumumerator(pm_s, 6)
         signal_s = am_s * np.exp(pm_s * 1j)
 
     return pm_f, pm_s
